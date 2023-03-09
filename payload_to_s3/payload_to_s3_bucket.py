b02f43fa220d468ef7368797450877c4a9dd13d2
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-03-09 12:54:15 -0800

    Added /csv path to s3 lambda, and payload function

--- payload_to_s3/payload_to_s3_bucket.py
+++ payload_to_s3/payload_to_s3_bucket.py
@@ -1,11 +1,32 @@
 import json
 import boto3
+import botocore
 import pandas as pd
+from datetime import datetime
 
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+    print(event)
     if event['rawPath'] == '/': #since the only goal for this lambda function is to get data we do not need an api gateway
+        BUCKET_NAME = 'jan-python-23-capstone-team3' #targeting this specific bucket
+        FILE_NAME = 'earthquake_data_' + datetime.utcnow().strftime('%Y%m%d%H%M%SZ') + '.json'
+        print(FILE_NAME)
+        
+        request_json = json.loads(event['body'])
+
+        try:
+            s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=json.dumps(request_json)) #this uploads the file to the bucket it will replace/update the file
+            return {
+                'statusCode': 200,
+                'body': "success"
+            }
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+    elif event['rawPath'] == '/csv': #since the only goal for this lambda function is to get data we do not need an api gateway
         BUCKET_NAME = 'jan-python-23-capstone-team3' #targeting this specific bucket
         
         request_json = json.loads(event['body'])
---
+++ payload_to_s3/send_csv_payload_to_lambda.py
@@ -0,0 +1,68 @@
+import json
+import math
+import types
+import zipfile
+import pandas as pd
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+DATA_CLEANING_PATH = 'data_cleaning'
+DATA_TYPE = 'Global_Earthquake_Data.csv'
+FILE_PATH = f'{DATA_CLEANING_PATH}\{DATA_TYPE}'
+COMPRESSED_FILE_PATH = f'{DATA_CLEANING_PATH}\{DATA_TYPE}.zip'
+
+URL = "https://mfcp436giirb7jwlvgizni2w7m0agrmc.lambda-url.us-west-1.on.aws/csv"
+ROWS_PER_PAYLOAD = 2500
+
+def unzip_zip_file():
+    with zipfile.ZipFile(COMPRESSED_FILE_PATH, 'r') as zip_ref:
+        zip_ref.extractall(DATA_CLEANING_PATH)
+
+def initialize_session() -> requests.Session:
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount(URL, adapter)
+    return session
+
+
+def get_file_name(num, rows=ROWS_PER_PAYLOAD) -> str:
+    return f"Global_Earthquake_Data_{rows}_part{num}.csv"
+
+
+def get_payload(payload_df: pd.DataFrame):
+    i = 0
+    num = 1
+    
+    rows_in_df = len(payload_df)
+    row_per_payload_limit = ((math.floor(rows_in_df / ROWS_PER_PAYLOAD)) * ROWS_PER_PAYLOAD)
+    
+    while i < row_per_payload_limit:
+        yield get_file_name(num), payload_df.iloc[i : i + ROWS_PER_PAYLOAD]
+        num += 1
+        i += ROWS_PER_PAYLOAD
+    else:
+        yield get_file_name(num, rows_in_df - i), payload_df.iloc[i:]
+
+
+def send_payload(payloads: types.GeneratorType, session: requests.Session):
+    for file_name, payload_df in payloads:
+        payload = {"file_name": file_name, "data": payload_df.to_json()}
+        r = session.post(url=URL, data=json.dumps(payload), headers={'Content-type': 'application/json'})
+        print(r)
+    
+    
+def main():
+    session = initialize_session()
+    
+    unzip_zip_file()
+    
+    earthquake_df = pd.read_csv(FILE_PATH)
+    payload_df_generator = get_payload(earthquake_df)
+    
+    send_payload(payload_df_generator, session)
+    
+
+if __name__ == "__main__":
+    main()
