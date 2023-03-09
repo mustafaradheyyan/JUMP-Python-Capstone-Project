977c0b1411b70ce96627ac16646eac26c84bee91
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-03-09 13:16:47 -0800

    Refactored code to make it smaller

--- payload_to_s3/payload_to_s3_bucket.py
+++ payload_to_s3/payload_to_s3_bucket.py
@@ -7,14 +7,12 @@ from datetime import datetime
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    print(event)
+    BUCKET_NAME = 'jan-python-23-capstone-team3' #targeting this specific bucket
+    request_json = json.loads(event['body'])
+    
     if event['rawPath'] == '/': #since the only goal for this lambda function is to get data we do not need an api gateway
-        BUCKET_NAME = 'jan-python-23-capstone-team3' #targeting this specific bucket
         FILE_NAME = 'earthquake_data_' + datetime.utcnow().strftime('%Y%m%d%H%M%SZ') + '.json'
-        print(FILE_NAME)
         
-        request_json = json.loads(event['body'])
-
         try:
             s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=json.dumps(request_json)) #this uploads the file to the bucket it will replace/update the file
             return {
@@ -26,19 +24,15 @@ def lambda_handler(event, context):
                 print("The object does not exist.")
             else:
                 raise
-    elif event['rawPath'] == '/csv': #since the only goal for this lambda function is to get data we do not need an api gateway
-        BUCKET_NAME = 'jan-python-23-capstone-team3' #targeting this specific bucket
+    elif event['rawPath'] == '/csv':
+        FILE_NAME = request_json['file_name']
         
-        request_json = json.loads(event['body'])
-        
-        FILE_NAME = request_json['file_name'] # replace with your object key
-    
         request_data = request_json['data']
         request_df = pd.read_json(request_data)
         request_csv = request_df.to_csv(index=False)
 
         try:
-            s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=request_csv) #this uploads the file to the bucket it will replace/update the file
+            s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=request_csv)
             return {
                 'statusCode': 200,
                 'body': "success"
