3babafb40bf753e5de49fef10a9d2f3c4559f4c2
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-03-09 12:57:53 -0800

    Added single data of .json file processing to rds

--- lambda_s3_to_rds/s3_to_rds.py
+++ lambda_s3_to_rds/s3_to_rds.py
@@ -31,30 +31,41 @@ except pymysql.MySQLError as e:
 
 logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
-
+def depth(L): return (isinstance(L, list) or isinstance(L, tuple)) and max(map(depth, L))+1
 
 def lambda_handler(event, context):
     print(event)#to check the data in event
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     file_name = event["Records"][0]["s3"]["object"]["key"]
     
-    csv_file = s3.get_object(Bucket=bucket_name, Key=file_name)
-    csv_content = csv_file["Body"].read().decode('utf-8')
+    file = s3.get_object(Bucket=bucket_name, Key=file_name)
+    content = file["Body"].read().decode('utf-8')
     
-    csv_string_io = StringIO(csv_content)
-    
-    csv_content_df = data_cleaning(pd.read_csv(csv_string_io))
-    csv_string_io = StringIO(csv_content_df.to_csv(index=False))
-    
-    reader = csv.reader(csv_string_io)
-    attributes_list = next(reader, None)
-    csv_data = [tuple(line) for line in reader]
+    if file_name.endswith(".csv"):
+        csv_string_io = StringIO(content)
+        
+        csv_content_df = data_cleaning(pd.read_csv(csv_string_io))
+        csv_string_io = StringIO(csv_content_df.to_csv(index=False))
+        
+        reader = csv.reader(csv_string_io)
+        attributes_list = next(reader, None)
+        data = [tuple(line) for line in reader]
+    elif file_name.endswith(".json"):
+        json_content = json.loads(content)
+        df_content = pd.DataFrame(json_content, columns = list(json_content.keys()))
+        json_content_df = data_cleaning(df_content)
 
+        attributes_list = list(json_content_df.columns)
+        data = json_content_df.iloc[0].values.flatten().tolist()
+        
     with conn.cursor() as cur:
         query = prep_insert_qry(attributes_list)
         try:
-            cur.fast_executemany = True
-            cur.executemany(query, csv_data)
+            if depth(data) > 1:
+                cur.fast_executemany = True
+                cur.executemany(query, data)
+            else:
+                cur.execute(query, data)
         except pymysql.err.IntegrityError as e:
             pass
         except Exception as e:
