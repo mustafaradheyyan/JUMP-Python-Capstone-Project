f250227d007dba0582c788222ad1e26eea5c6276
mustafaradheyyan <31796217+mustafaradheyyan@users.noreply.github.com>
2023-06-21 21:36:56 -0700

    Update db_funcs.py

--- get_earthquake_data/db_funcs.py
+++ get_earthquake_data/db_funcs.py
@@ -1,7 +1,14 @@
 TABLE_NAME = "seismic_event"
 
 def get_rows(num_of_rows):
-    sql_statement = f'''SELECT * FROM {TABLE_NAME}
-    LIMIT {num_of_rows};
+
+    sql_statement = f'''
+    SELECT *
+        FROM (
+            SELECT *, ROW_NUMBER() OVER (PARTITION BY type) AS n
+            FROM {TABLE_NAME}
+             ) AS x
+        WHERE n <= {num_of_rows};
     '''
+    
     return sql_statement
